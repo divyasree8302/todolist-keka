a7b3b862a7539c88d83d17a3fa8ef5fff17a22ad
divyasree8302 <114393694+divyasree8302@users.noreply.github.com>
2024-01-10 11:59:38 +0530

    Add files via upload

---
+++ SimpleTodo-main(keka)/SimpleTodo/SimpleTodoApp.swift
@@ -0,0 +1,14 @@
+
+import SwiftUI
+
+@main
+struct SimpleTodoApp: App {
+    
+    let persistentContainer = CoreDataManager.shared.persistentContainer
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView().environment(\.managedObjectContext, persistentContainer.viewContext)
+        }
+    }
+}
